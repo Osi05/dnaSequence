a1fd954f549cdecccc8ce5e31ca2108b3e2f2dfa
Callistus <c.o.enekhaze@gmail.com>
2025-03-18 22:03:33 -0400

    working on the sequence

---
+++ src/main/java/com/bptn/dnaSequence/DNA.java
@@ -0,0 +1,25 @@
+package com.bptn.dnaSequence;
+
+public class DNA {
+	
+	/*
+	 Writing a DNA java program that determines whether there is a protein in a strand of DNA
+	 */
+	
+	public static void main(String[] args) {
+		
+		String dna1 = "ATGCGATACGCTTGA";
+		String dna2 = "ATGCGATACGTGA";
+		String dna3 = "ATTAATATGTACTGA";
+		
+		//creating dna and assigning dna2 to it
+		
+		String dna = dna2;
+		
+		//printing out length of dna
+		
+		System.out.println("Length of dna is " + dna.length());
+		
+	}
+
+}
